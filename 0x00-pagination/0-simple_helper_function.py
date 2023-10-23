1ff0d157141edf57898721b4d2ddefde770701da
Sylvain T <sylvaintagnabou@gmail.com>
2023-10-23 18:01:15 +0000

    0x01

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ from typing import Tuple
 def index_range(page, page_size):
     """
     Return a tuple of the start and end index for a given page and page size.
-    
+
     :param page: The current page number (1-indexed).
     :param page_size: The number of items per page.
     :return: A tuple of (start_index, end_index).
@@ -20,4 +20,3 @@ def index_range(page, page_size):
     end_index = start_index + page_size
 
     return start_index, end_index
-
