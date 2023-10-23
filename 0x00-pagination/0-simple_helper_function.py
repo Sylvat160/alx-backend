15967f435e035fc774919d4335fc8c94526c1dee
Sylvain T <sylvaintagnabou@gmail.com>
2023-10-23 17:57:24 +0000

    0x01 task00

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Pagination helper function.
+"""
+from typing import Tuple
+
+
+def index_range(page, page_size):
+    """
+    Return a tuple of the start and end index for a given page and page size.
+    
+    :param page: The current page number (1-indexed).
+    :param page_size: The number of items per page.
+    :return: A tuple of (start_index, end_index).
+    """
+    if page <= 1:
+        start_index = 0
+    else:
+        start_index = (page - 1) * page_size
+
+    end_index = start_index + page_size
+
+    return start_index, end_index
+
