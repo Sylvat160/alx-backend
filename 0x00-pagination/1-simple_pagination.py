73d3de2b21779305133673aa8fd1c0a0df66cb7b
Sylvain T <sylvaintagnabou@gmail.com>
2023-10-23 18:12:07 +0000

    0x00 task01

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,22 +5,12 @@ import csv
 from typing import List, Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
     """
-    Return a tuple of the start and end index for a given page and page size.
-
-    :param page: The current page number (1-indexed).
-    :param page_size: The number of items per page.
-    :return: A tuple of (start_index, end_index).
-    """
-    if page <= 1:
-        start_index = 0
-    else:
-        start_index = (page - 1) * page_size
-
-    end_index = start_index + page_size
-
-    return start_index, end_index
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -47,15 +37,10 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Retrieves a page of data.
         """
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
-
-        # assert type(page) == int and type(page_size) == int
-        # assert page > 0 and page_size > 0
-        start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-
-        if start_index >= len(dataset):
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
-
-        return dataset[start_index:end_index]
+        return data[start:end]
